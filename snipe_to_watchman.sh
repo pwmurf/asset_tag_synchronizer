c3f80cdd75364ae53cea4c610d44bcd9883046ea
MetroEastPeter <peter@metroeast.org>
2024-01-09 16:40:00 -0800

    quick fix

--- snipe_to_watchman.sh
+++ snipe_to_watchman.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/zsh
 #
-# add asset tags from Snipe to devices in Watchman Monitoring Client 
+# synchronize asset tags from SnipeIT to macOS devices in Watchman Monitoring Client 
 
 
-# Watchman API Key & URI
-watch_key='UW_bE14wUmaHfC4RLJDjvjbnmdKZsYiENZVJEA'
-watch_URI='https://metroeast.monitoringclient.com/v2.5/computers'
+# add Watchman API key & URI
+watch_key=''
+watch_URI=''
 
-# Snipe IT API Key
-Snipe_API_key='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNWI5Njk3M2U1NGQwZTVlMjdhZDIxOGY5N2M1YTEwYWRhZmRjNjZkZWUzNzFkMmQ1YjNkZjlkZWQ5YzUyYWZmN2Y2OWIyYWU2MjYxYjBkNjYiLCJpYXQiOjE2MTQ2NDY4NDQsIm5iZiI6MTYxNDY0Njg0NCwiZXhwIjoyMjQ1Nzk4ODQ0LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.uoxI615Iv28MamV9qvvcVN6sbKrVmsFGFj1dsJb_bQBAgSyOYRCjeThmF5FAMENotL7HMMbbIIMrvS7dEV-ceGCErD2CK6TJdnBPFLBaJehjnDtufjBbIt_dg2suIovuo-SkGL7RL58nc3LJuGEYbE32_UcVbSjcJLrHi8fhkShxwSCGjwz34E7Fh9IITBCVP7F5ag6Btsni_L1T6CiSMpVWfsaGmFaPLN2cCkKXOUZ62PPAeNI4lv67pnueo5R3x6-00A6YoWEJGBG7Sq7YedEoZ1BcFjuGDCCuyrsGmQ4-RD0Z786OyZOeJc3MzgmXdfcl04I0pXvZDEDOKG_CSPaUbe1fR-PvOIPewvd8mn1tthjNMXxDPla8gm9qE2o-MdOxS7jmh_ATA_aqIsrx5aDl4PGRuF8OoB9dgp2gIllrbxdFZxx5xuOj4Ud1J1FxViAZp5KhvpbYDHqTAb4Ri7DoygyVf0wR9iYaYEECj5oqyRd9DtOxtRXY4cb82FD-CMGEBzBZtszvnj5FJ25MSbdef5u3qy4rFaSzEle7wXmI187HuwdBiZjs5ZAgK2Qhz4bLkWReWqD0VFSYj5IrvHf9dODSyzozC2ca-imWT0w7HcIVbo4xKM8XM53V87qHEYkR09eYKh88HW-SMcTAK3YX35PIvr9loOZz0cXqxmg'
+# add Snipe IT API key & URI
+Snipe_API_key=''
+SNIPE_SERVER=''
 
-SNIPE_SERVER='metroeast.snipe-it.io'
 HEAD_key="Authorization: Bearer $Snipe_API_key"
 HEAD_acc='Accept: application/json'
 HEAD_typ='Content-Type: application/json'
@@ -68,3 +68,5 @@ done < <(
   ## parse out id & serial
   jq '.[] | {id: .watchman_id, serial: .serial_number} | .id, .serial' <<< "$watchman_info" 
 )
+
+exit 0
